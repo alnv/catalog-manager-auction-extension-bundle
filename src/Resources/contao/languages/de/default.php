f181b70d5af5faca9451e356dc462bfb8851f63d
Alexander Naumov <me@alexandernaumov.de>
2019-04-24 08:56:57 +0200

    add labels

--- src/Resources/contao/languages/de/default.php
+++ src/Resources/contao/languages/de/default.php
@@ -5,3 +5,8 @@ $GLOBALS['TL_LANG']['MSC']['warningMessage'] = 'Ihr neues Gebot beträgt mehr al
 $GLOBALS['TL_LANG']['MSC']['errorMessage'] = 'Ihre Anfrage konnte nicht verarbeitet werden.';
 $GLOBALS['TL_LANG']['MSC']['successMessage'] = 'Ihr Gebot wurde erfolgreich übertragen.';
 $GLOBALS['TL_LANG']['MSC']['minOfferMessage'] = 'Der Mindestgebot liegt bei %s.';
+$GLOBALS['TL_LANG']['MSC']['deleteMessage'] = 'Ihr Gebot wurde erfolgreich gelöscht.';
+
+$GLOBALS['TL_LANG']['MSC']['slabel'] = 'Bieten';
+$GLOBALS['TL_LANG']['MSC']['dlabel'] = 'Gebote zurücknehmen';
+$GLOBALS['TL_LANG']['MSC']['plabel'] = 'Geben Sie Ihr Gebot ein';
