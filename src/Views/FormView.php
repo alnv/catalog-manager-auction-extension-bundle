40145b9da355ae93f4511e91a4696a28f4e5b20e
Alexander Naumov <me@alexandernaumov.de>
2019-04-24 08:56:35 +0200

    add delete button

--- src/Views/FormView.php
+++ src/Views/FormView.php
@@ -19,10 +19,30 @@ class FormView {
 
     public function parse() {
 
+        $intUserId = 0;
+        $objDatabase = \Database::getInstance();
         $objTemplate = new \FrontendTemplate( 'auction_view_form' );
 
+        $objTemplate->hasOffers = false;
         $objTemplate->id = $this->strId;
         $objTemplate->table = $this->strTable;
+
+        $objTemplate->sLabel = $GLOBALS['TL_LANG']['MSC']['slabel'];
+        $objTemplate->dLabel = $GLOBALS['TL_LANG']['MSC']['dlabel'];
+        $objTemplate->pLabel = $GLOBALS['TL_LANG']['MSC']['plabel'];
+
+        if ( FE_USER_LOGGED_IN ) {
+
+            $objUser = \FrontendUser::getInstance();
+            $intUserId = $objUser->id;
+        }
+
+        $objEntity = $objDatabase->prepare('SELECT * FROM cm_offers WHERE member = ? AND offer_to = ? AND offer_object = ? ORDER BY tstamp DESC')->limit(1)->execute( $intUserId, $this->strId, $this->strTable );
+
+        if ( $objEntity->numRows ) {
+
+            $objTemplate->hasOffers = true;
+        }
 
         return $objTemplate->parse();
     }
